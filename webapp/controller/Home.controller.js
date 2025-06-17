73c8a50f7cd5d41417b4f8748e1142148ddaaf2e
ITS-MATERA <its.developer.matera@gmail.com>
2025-06-17 17:20:22 +0200

    dev: cose

--- webapp/controller/Home.controller.js
+++ webapp/controller/Home.controller.js
@@ -1,7 +1,11 @@
-sap.ui.define(["./BaseController"], (BaseController) => {
+sap.ui.define(["./BaseController"], (BaseController, entityUtils) => {
   "use strict";
 
   return BaseController.extend("appnamespace.controller.Home", {
-    onInit() {},
+    onInit() {
+      this.getRouter().getRoute("home").attachPatternMatched(this._onObjectMatched, this);
+    },
+
+    _onObjectMatched: async function () {},
   });
 });
