4bf013bd19cbdbfb3c53cb1fa6481d4e9fcf49b6
ITS-MATERA <its.developer.matera@gmail.com>
2025-06-18 13:02:23 +0200

    dev: inserito controller test

---
+++ webapp/controller/Test.controller.js
@@ -0,0 +1,13 @@
+sap.ui.define(["./BaseController"], (BaseController) => {
+  "use strict";
+
+  return BaseController.extend("appnamespace.controller.Test", {
+    onInit() {
+      this.getRouter().getRoute("test").attachPatternMatched(this._onObjectMatched, this);
+    },
+
+    _onObjectMatched: async function () {
+      console.log("£CIAO");
+    },
+  });
+});
